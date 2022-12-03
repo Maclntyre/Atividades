95876c026461f9c694bd0829004af9fc4bd196cd
Geri <gerimaclntyre@gemail.com>
2022-12-03 20:24:18 -0300

    Manter a calma é essencial

---
+++ Fundamentos/secao-04-intro-JavaScript/Js-primeiros-passos/switch-case.js
@@ -0,0 +1,15 @@
+let candidata = aprovada;
+switch (candidata) {
+
+    case 'aprovada':
+      console.log('Parabéns, você foi aprovada(o)!');
+      break;
+    case 'lista':
+    console.log('Você está na nossa lista de espera'); 
+      break;
+     case 'reprovado':
+      console.log('Você foi reprovada(o)');
+      break;
+      default:
+        console.log('informação incorreta');
+}
